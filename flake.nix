6abfa316e3febefdd23157b0e1761c3cdd021526
PeaterPita <90217572+PeaterPita@users.noreply.github.com>
2026-07-05 15:21:38 +0100

    First Attempt Icon ++ Nix ++ Temp Icon created ++ Nix package output that builds required icons ++ manifest.json icons expanded

--- flake.nix
+++ flake.nix
@@ -18,5 +18,30 @@
           web-ext
         ];
       };
+
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        pname = "plata";
+        version = "0.1";
+
+        src = ./src;
+
+        nativeBuildInputs = with pkgs; [
+          inkscape
+          web-ext
+        ];
+
+        buildPhase = ''
+          for size in 32 48 96 128; do 
+              inkscape --export-type=png --export-width=$size --export-filename=icons/plata-$size.png icons/Plata.svg
+          done
+        '';
+
+        installPhase = ''
+          mkdir -p $out
+          HOME=$TMPDIR web-ext build --source-dir . --artifacts-dir $out --overwrite-dest --ignore-files "icons/Hero.svg" --ignore-files "icons/Plata.svg"
+        '';
+
+      };
+
     };
 }
